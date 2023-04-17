8eac5797a8a3bcab2b119680fecad297ad8227c1
Martin Hoejmann <martin.hojmann@agillic.com>
2023-04-17 16:09:12 +0200

    GDT Component

--- src/routes/admin/gdt/[slug]/+page.svelte
+++ src/routes/admin/gdt/[slug]/+page.svelte
@@ -1,62 +1,8 @@
 <script lang="ts">
-	import type { PageServerData, PageServerLoad } from './$types';
-	import {
-		Button,
-		Column,
-		Grid,
-		ImageLoader,
-		Row,
-		TextArea,
-		TextInput
-	} from 'carbon-components-svelte';
+	import GlobalDataTableForm from '$lib/components/GlobalDataTableForm.svelte';
+	import type { PageServerData } from './$types';
 
 	export let data: PageServerData;
 </script>
 
-<form method="post">
-	<Grid>
-		<Row padding>
-			<Column>
-				<ImageLoader src="https://{data.gdtRecord.IMAGE_URL}" />
-				<input type="hidden" name="recordId" value={data.gdtRecord.ID} />
-			</Column>
-		</Row>
-		<Row padding>
-			<Column>
-				<TextInput
-					labelText="Title"
-					placeholder="Title"
-					name="title"
-					value={data.gdtRecord.TITLE ?? ''}
-				/>
-				<input type="hidden" name="personID" value={data.gdtRecord.ID} />
-			</Column>
-		</Row>
-		<Row padding>
-			<Column>
-				<TextInput
-					labelText="Category"
-					placeholder="Category"
-					name="category"
-					value={data.gdtRecord.CATEGORY ?? ''}
-				/>
-				<input type="hidden" name="personID" value={data.gdtRecord.ID} />
-			</Column>
-		</Row>
-		<Row padding>
-			<Column>
-				<TextArea
-					labelText="Description"
-					placeholder="Description"
-					name="description"
-					value={data.gdtRecord.DESCRIPTION ?? ''}
-				/>
-			</Column>
-		</Row>
-		<Row padding>
-			<Column>
-				<Button type="submit">Save</Button>
-			</Column>
-		</Row>
-	</Grid>
-</form>
+<GlobalDataTableForm globalDataTableRecord={data} />
