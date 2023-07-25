c1d44770227c3c2b75e1b6b6e736d8c7aeb3a040
Martin Hoejmann <martin.hojmann@agillic.com>
2023-07-25 09:22:20 +0200

    Added styling and instruction text to iframe page

--- src/routes/admin/iframe/+page.svelte
+++ src/routes/admin/iframe/+page.svelte
@@ -1,19 +1,27 @@
 <script lang="ts">
-	import { Column, Grid, Row } from 'carbon-components-svelte';
-	import type { PageData } from './$types';
+    import {Column, Grid, Row} from 'carbon-components-svelte';
+    import type {PageData} from './$types';
 
-	export let data: PageData;
+    export let data: PageData;
 </script>
 
-<Grid>
-	<Row padding>
-		<Column>
-			<iframe
-				src="https://web.cafeconnect.net/web/namedservice/profilemanagement?lgn_uid={data.lgn_uid}"
-				title="iframe"
-				width="700"
-				height="500"
-			/>
-		</Column>
-	</Row>
+<Grid fullwidth style="display: flex; justify-content: center">
+    <Row>
+        <Column>
+            <h2>Put "?lgn_uid=" + AGILLIC_ID in the url</h2>
+        </Column>
+    </Row>
+</Grid>
+
+<Grid fullwidth style="display: flex; justify-content: center">
+    <Row padding>
+        <Column>
+            <iframe
+                    src="https://web.cafeconnect.net/web/namedservice/profilemanagement?lgn_uid={data.lgn_uid}"
+                    title="iframe"
+                    width="700"
+                    height="500"
+            />
+        </Column>
+    </Row>
 </Grid>
